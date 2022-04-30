2a531698c2330597cec50a83a426d63207428ccd
Chaewon Song <81732426+chaewonS@users.noreply.github.com>
2022-04-30 15:20:32 +0900

    Update make_pair_json.py

--- all/make_pair_json.py
+++ all/make_pair_json.py
@@ -7,12 +7,12 @@ Beacon = dict()
 beacon1 = dict()
 beacon1["location"] = "12,29"
 beacon1["place"] = "교수님랩실"
-beacon1["group"] = (12,28)
+beacon1["group"] = (12,29)
 Beacon["00:19:01:70:81:33"] = beacon1#
 beacon2 = dict()
 beacon2["location"] = "12,29"
 beacon2["place"] = "좌상단"
-beacon2["group"] = (12,28)
+beacon2["group"] = (12,29)
 Beacon["00:19:01:70:80:d9"] = beacon2#
 
 
@@ -43,23 +43,23 @@ Beacon["00:19:01:70:85:0f"] = beacon6
 beacon7 = dict()
 beacon7["location"] = "10,19"
 beacon7["place"] = "4층EV"
-beacon7["group"] = (15,16)
+beacon7["group"] = (10,19)
 Beacon["00:19:01:70:81:75"] = beacon7#
 beacon8 = dict()
 beacon8["location"] = "10,19"
 beacon8["place"] = "좌상단"
-beacon8["group"] = (15,16)
+beacon8["group"] = (10,19)
 Beacon["00:19:01:70:84:4b"] = beacon8 
 
 beacon9 = dict()
 beacon9["location"] = "11,18"
 beacon9["place"] = "402(C)"
-beacon9["group"] = (12,18)
+beacon9["group"] = (11,18)
 Beacon["00:19:01:70:82:cb"] = beacon9
 beacon10 = dict()
 beacon10["location"] = "11,18"
 beacon10["place"] = "좌상단"
-beacon10["group"] = (12,18)
+beacon10["group"] = (11,18)
 Beacon["00:19:01:70:86:2e"] = beacon10
 
 with io.open('./pair.json', 'wb',
